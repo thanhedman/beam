87449113ec0f5d3a531a32b4d723808179fcff0e
haitam <10712736+htmlrb@users.noreply.github.com>
2019-08-30 09:44:54 -0700

    greedy4

--- src/main/scala/beam/agentsim/agents/ridehail/AsyncAlonsoMoraAlgForRideHail.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/AsyncAlonsoMoraAlgForRideHail.scala
@@ -115,8 +115,8 @@ class AsyncAlonsoMoraAlgForRideHail(
             }
             .foreach { t =>
               val cost = computeCost(t, v)
-              potentialTripsWithKPassengers.append((t, v, cost))
-              /*if (potentialTripsWithKPassengers.size == 2) {
+              //potentialTripsWithKPassengers.append((t, v, cost))
+              if (potentialTripsWithKPassengers.size == solutionSpaceSizePerVehicle*k) {
                 // then replace the trip with highest sum of delays
                 val ((_, _, tripWithLargestDelayCost), index) =
                   potentialTripsWithKPassengers.filter(_._1.requests.size == k).zipWithIndex.maxBy(_._1._3)
@@ -126,7 +126,7 @@ class AsyncAlonsoMoraAlgForRideHail(
               } else {
                 // then add the new trip
                 potentialTripsWithKPassengers.append((t, v, cost))
-              }*/
+              }
             }
       }
       potentialTrips.appendAll(potentialTripsWithKPassengers)
